c76173a95064c407acd810e6cf4b20a02b7b45df
Nikolay Sheshin <nasheshin.portfolio@gmail.com>
2024-03-02 17:10:39 +0500

    Handling exceptions

--- streamlit_page.py
+++ streamlit_page.py
@@ -23,27 +23,30 @@ def load_image():
 
 
 def main():
-    st.title(localizeconst.IMAGE_CLASSIFICATION_TITLE)
-    img = load_image()
-    labels_input = st.text_input(localizeconst.ENTER_CLASSES_INSTRUCTION)
-   
-    run_button = st.button(localizeconst.LAUNCH_CLASSIFICATION)
-    if not run_button:
-        return
+    try:
+        st.title(localizeconst.IMAGE_CLASSIFICATION_TITLE)
+        img = load_image()
+        labels_input = st.text_input(localizeconst.ENTER_CLASSES_INSTRUCTION)
     
-    labels = labels_input.split(',')
-    if len(labels) <= 0 or labels[0] == '':
-        st.error(localizeconst.NO_CLASSES_ERROR)
-        return
+        run_button = st.button(localizeconst.LAUNCH_CLASSIFICATION)
+        if not run_button:
+            return
+        
+        labels = labels_input.split(',')
+        if len(labels) <= 0 or labels[0] == '':
+            st.error(localizeconst.NO_CLASSES_ERROR)
+            return
 
-    if img is None:
-        st.error(localizeconst.NO_IMAGE_ERROR)
-        return
+        if img is None:
+            st.error(localizeconst.NO_IMAGE_ERROR)
+            return
 
-    model = load_clip_classifier()
-    with st.spinner(localizeconst.SPINNER_WAITING):
-        label, score = model.predict(img, labels)
-        st.write(localizeconst.CLASSIFICATION_RESULT_LABEL.format(label, int(score * 100)))
+        model = load_clip_classifier()
+        with st.spinner(localizeconst.SPINNER_WAITING):
+            label, score = model.predict(img, labels)
+            st.write(localizeconst.CLASSIFICATION_RESULT_LABEL.format(label, int(score * 100)))
+    except Exception as e:
+        st.error(e)
 
 
 if __name__ == "__main__":
